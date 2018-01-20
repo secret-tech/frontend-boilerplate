d0227214ee310cc1826ea7e1db2749f6971fc1b9
Aidar Ibatullin <amazing.space.invader@gmail.com>
2018-01-20 13:43:20 +0500

    Remove i18next

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import { Provider } from 'react-redux';
-// import { I18nextProvider } from 'react-i18next';
 import { AppContainer } from 'react-hot-loader';
 
 import 'normalize.css';
-// import 'reset.css';
 import './assets/main.css';
 import './assets/fonts/Roboto/stylesheet.css';
 
 // import configureStore from './redux/configureStore';
-// import i18next from './utils/i18next';
 import App from './containers/App';
 
 // const store = configureStore({});
