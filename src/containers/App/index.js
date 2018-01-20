0073fd44bbd267196b25ec24beb0d158056659d5
Aidar Ibatullin <amazing.space.invader@gmail.com>
2018-01-20 11:26:30 +0500

    Image loader

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import s from './styles.css';
 
-const Main = () => <div className={s.main}>Hello, world!</div>;
+const Main = () => (
+  <div className={s.main}>
+    <img src={require('./images/chvrches.jpg')}/>
+    <div>Hello, world!</div>
+  </div>
+);
 
 export default Main;
